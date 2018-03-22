99837c43885a6965dc71cdddcbdde905aaecdfb2
lukaszsl <lukasz.sliczner@interia.pl>
2018-03-22 22:31:36 +0000

    feat: Add event listener to play again button

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ const deck = document.querySelector('.deck');
 const modal = document.querySelector('.modal');
 const moves = document.querySelector('.moves');
 const movesNumber = document.querySelector('.moves-number');
+const playAgainBtn =document.querySelector('.modal__button');
 
 //Create a list that holds all of your cards
 const cards = document.getElementsByClassName('card');
@@ -71,6 +72,8 @@ deck.addEventListener('click', function(event) {
 		finalMessage();
 	}
 });
+
+playAgainBtn.addEventListener('click', newGame());
 
 // Add the open card to the list of open cards
 function addToList(card) {
